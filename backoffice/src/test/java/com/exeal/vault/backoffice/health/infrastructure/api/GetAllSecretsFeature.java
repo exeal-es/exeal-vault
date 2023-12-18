c77ed3848cc119ffea78a299cea0a6919ea02af5
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-12-18 18:49:04 +0100

    test: create new test get all secrets

---
+++ backoffice/src/test/java/com/exeal/vault/backoffice/health/infrastructure/api/GetAllSecretsFeature.java
@@ -0,0 +1,19 @@
+package com.exeal.vault.backoffice.health.infrastructure.api;
+
+import org.junit.jupiter.api.Test;
+
+public class GetAllSecretsFeature {
+
+  @Test
+  public void  getAllSecrets(){
+    //Given
+    //Dado que hay un secreto con la clave api_secret y el valor 123456
+
+    //when
+    //Cuando se piden todos los secretos
+
+    //Then
+    //Se devuelve un secreto con la clave api_secret y el valor 123456
+  }
+
+}
