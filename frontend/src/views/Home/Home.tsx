d5157dc0432bb8b055d69a48bf26ccc956ef5418
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-12-22 17:55:25 +0100

    feat: add feth

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './_home.css';
 
 function Home() {
+  const [secrets, setSecrets] = React.useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8080/secrets')
+      .then(res => res.json())
+      .then(data => setSecrets(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+       {secrets.map((secret: {key: string, value: string}) => <><p key={secret.key}>{secret.key}</p><p key={secret.value}>{secret.value}</p></>)}
         <a
           className="App-link"
           href="https://reactjs.org"
