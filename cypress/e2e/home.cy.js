19b3dc0bab2fb2f799318abb4120755cc4665a9e
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-12-22 17:23:14 +0100

    test: add test should display secret wip

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,13 @@
 describe('Home', () => {
 
-    it('should navigate to home', () => {
+    it('should display secrets', () => {
         cy.visit('http://localhost:80/')
         cy.url().should('include', 'localhost')
+
+        cy.get('p').contains('api_secret')
+        cy.get('p').contains('123456')
     })
+
+
 })
+
